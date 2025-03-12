4ab72ed03cedf2a301888e7c300d75c2beb4792c
luca <lucac375@gmail.com>
2025-03-12 20:17:51 +0200

    feat: add DataLink - no executors - we go agin

--- Duo/Data/DatabaseConnection.cs
+++ Duo/Data/DatabaseConnection.cs
@@ -1,15 +1,52 @@
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Data;
 
-public DataLink 
-{
-    SqlConnection sqlConnection; 
+public class DataLink
+{   
+    private SqlConnection sqlConnection;
+    private readonly string connectionString;
 
-    public DataLink 
+    public DataLink(IConfiguration configuration)
     {
-        sqlConnection = new sqlConnection("DataSource=np:\\.\pipe\LOCALDB#D6D39D8B\tsql\query;" +
-                                        "Initial Catalog=Categories; " +
-                                        "IntegratedSecurity=True"
-                                        "TrustServerCertificate=True")
+        string localDataSource = configuration["LocalDataSource"];
+        connectionString = "DataSource=" + localDataSource +
+                        "Initial Catalog=ISS_Duo; " +
+                        "IntegratedSecurity=True" +
+                        "TrustServerCertificate=True";
+
+        try 
+        {
+            sqlConnection = new sqlConnection(connectionString);
+        } 
+        catch (Exception ex)
+        {
+            throw new Exception($"Error initializing SQL connection: {ex.Message}");
+        }
     }
+
+    public void OpenConnection()
+    {
+        if(sqlConnection.State != ConnectionState.Open) 
+        {
+            sqlConnection.Open();
+        }
+    }
+
+    public void CloseConnection()
+    {
+        if(sqlConnection.State != ConnectionState.Closed) 
+        {
+            sqlConnection.Closed();
+        }
+    }
+    
+    DataLink: 
+    1. ExecuteScalar - single value - Get number of posts by category
+    2. ExecuteReader - dataTable - Reading, Filtering, Searching
+    3. ExecuteNonQuery - Insert, Delete, ... -> CRUD
+
+    Repository: 
+    dataLink.ExecuteScalar(storedprocname, params);
 }
