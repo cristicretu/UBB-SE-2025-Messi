da4280cbc9d3bad5dc40ad714d4e3616f418e038
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-03-12 19:32:22 +0200

    category page

--- Duo/Views/Pages/CategoryPage.xaml.cs
+++ Duo/Views/Pages/CategoryPage.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
 
 namespace Duo.Views.Pages
 {
@@ -7,6 +10,69 @@ namespace Duo.Views.Pages
         public CategoryPage()
         {
             this.InitializeComponent();
+            
+            // Set initial page when component loads
+            contentFrame.Navigate(typeof(Page)); // Navigate to a default page
+            
+            // Subscribe to NavigationView events
+            nvSample.ItemInvoked += NvSample_ItemInvoked;
+            nvSample.BackRequested += NvSample_BackRequested;
+        }
+        
+        private void NvSample_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                // Handle settings navigation if needed
+                // contentFrame.Navigate(typeof(SettingsPage));
+                return;
+            }
+            
+            // Get the tag of the selected item
+            var tag = args.InvokedItemContainer?.Tag?.ToString();
+            
+            // Navigate based on the tag
+            NavigateToPage(tag);
+        }
+        
+        private void NvSample_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (contentFrame.CanGoBack)
+            {
+                contentFrame.GoBack();
+            }
+        }
+        
+        private void NavigateToPage(string pageTag)
+        {
+            Type? pageType = null;
+            
+            // Map the tag to the appropriate page type
+            switch (pageTag)
+            {
+                case "SamplePage1":
+                    // pageType = typeof(SamplePage1);
+                    break;
+                case "SamplePage2":
+                    // pageType = typeof(SamplePage2);
+                    break;
+                case "SamplePage3":
+                    // pageType = typeof(SamplePage3);
+                    break;
+                case "SamplePage4":
+                    // pageType = typeof(SamplePage4);
+                    break;
+                default:
+                    // Default page if no match is found
+                    // pageType = typeof(DefaultPage);
+                    break;
+            }
+            
+            // Navigate to the page if a type was set
+            if (pageType != null)
+            {
+                contentFrame.Navigate(pageType);
+            }
         }
     }
 }
