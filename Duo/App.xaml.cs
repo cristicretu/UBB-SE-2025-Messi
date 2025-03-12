fd047acb064ea2a3f49ed87f61c4b13244d87b52
Cristian Crețu <crisemcr@gmail.com>
2025-03-12 19:06:04 +0200

    feat: add mockup login screen (#5)

--- Duo/App.xaml.cs
+++ Duo/App.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using Duo.Views;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -41,10 +42,24 @@ namespace Duo
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            // show the login window first
+            var loginWindow = new LoginWindow();
+            loginWindow.LoginSuccessful += LoginWindow_LoginSuccessful;
+            loginWindow.Activate();
+            
+            m_loginWindow = loginWindow;
+        }
+        
+        private void LoginWindow_LoginSuccessful(object? sender, EventArgs e)
+        {
+            // show the main window, after login
             m_window = new MainWindow();
             m_window.Activate();
+            
+            m_loginWindow = null;
         }
 
         private Window? m_window;
+        private LoginWindow? m_loginWindow;
     }
 }
