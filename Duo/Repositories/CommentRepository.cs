2226d11988b93f35ab16012f64042dc2de1b58cb
Vasile Draguta <dragutavasile@gmail.com>
2025-03-14 16:31:20 +0200

    refactored methods acording to uml

--- Duo/Repositories/CommentRepository.cs
+++ Duo/Repositories/CommentRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Data.SqlClient;
 
 public class CommentRepository
@@ -11,7 +12,68 @@ public class CommentRepository
         _dataLink = dataLink;
     }
 
-    public int CreateComment(Comment comment)
+public Comment GetCommentById(int id)
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+        new SqlParameter("@CommentID", id)
+        };
+
+        try
+        {
+            var dataTable = _dataLink.ExecuteReader("GetCommentByID", parameters);
+            var row = dataTable.Rows[0];
+
+            return new Comment(
+                Convert.ToInt32(row[0]),
+                row[1]?.ToString() ?? string.Empty,
+                Convert.ToInt32(row[2]),
+                Convert.ToInt32(row[3]),
+                row[4] == DBNull.Value ? 0 : Convert.ToInt32(row[4]),
+                Convert.ToDateTime(row[5]),
+                Convert.ToInt32(row[6]),
+                Convert.ToInt32(row[7])
+            );
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public List<Comment> GetCommentsByPostId(int postId)
+    {
+        List<Comment> comments = new List<Comment>();
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new SqlParameter("@PostID", postId)
+        };
+        try
+        {
+            var dataTable = _dataLink.ExecuteReader("GetCommentsByPostID", parameters);
+            foreach (System.Data.DataRow row in dataTable.Rows)
+            {
+                Comment comment = new Comment(
+                    Convert.ToInt32(row[0]),
+                    row[1]?.ToString() ?? string.Empty,
+                    Convert.ToInt32(row[2]),
+                    Convert.ToInt32(row[3]),
+                    row[4] == DBNull.Value ? 0 : Convert.ToInt32(row[4]),
+                    Convert.ToDateTime(row[5]),
+                    Convert.ToInt32(row[6]),
+                    Convert.ToInt32(row[7])
+                );
+                comments.Add(comment);
+            }
+            return comments;
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+public Comment CreateComment(Comment comment)
     {
         SqlParameter[] parameters = new SqlParameter[]
         {
@@ -24,7 +86,8 @@ public class CommentRepository
         try
         {
             int? result = _dataLink.ExecuteScalar<int>("CreateComment", parameters);
-            return result ?? 0;
+            comment.Id = result.Value;
+            return comment;
         }
         catch (SqlException ex)
         {
@@ -32,45 +95,39 @@ public class CommentRepository
         }
     }
 
-    public void DeleteComment(int id)
+    public bool DeleteComment(int id)
     {
         SqlParameter[] parameters = new SqlParameter[]
         {
             new SqlParameter("@CommentID", id)
         };
-        _dataLink.ExecuteNonQuery("DeleteComment", parameters);
+        try
+        {
+            _dataLink.ExecuteNonQuery("DeleteComment", parameters);
+            return true;
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
-    public void UpdateComment(Comment comment)
+    public bool UpdateComment(Comment comment)
     {
         SqlParameter[] parameters = new SqlParameter[]
         {
             new SqlParameter("@CommentID", comment.Id),
             new SqlParameter("@NewContent", comment.Content),
         };
-        _dataLink.ExecuteNonQuery("UpdateComment", parameters);
-    }
-
-    public List<Comment> GetAllComments()
-    {
-        List<Comment> comments = new List<Comment>();
-        var dataTable = _dataLink.ExecuteReader("GetAllComments");
-
-        foreach (System.Data.DataRow row in dataTable.Rows)
+        try
         {
-            Comment comment = new Comment(
-                Convert.ToInt32(row[0]),
-                row[1]?.ToString() ?? string.Empty,
-                Convert.ToInt32(row[2]),
-                Convert.ToInt32(row[3]),
-                row[4] == DBNull.Value ? 0 : Convert.ToInt32(row[4]),
-                Convert.ToDateTime(row[5]),
-                Convert.ToInt32(row[6]),
-                Convert.ToInt32(row[7])
-            );
-            comments.Add(comment);
+            _dataLink.ExecuteNonQuery("UpdateComment", parameters);
+            return true;
         }
-        return comments;
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public List<Comment> GetReplies(int parentCommentId)
@@ -80,23 +137,48 @@ public class CommentRepository
         {
             new SqlParameter("@ParentCommentID", parentCommentId)
         };
-        var dataTable = _dataLink.ExecuteReader("GetReplies", parameters);
 
-        foreach (System.Data.DataRow row in dataTable.Rows)
+        try
         {
-            Comment comment = new Comment(
-                Convert.ToInt32(row[0]),
-                row[1]?.ToString() ?? string.Empty,
-                Convert.ToInt32(row[2]),
-                Convert.ToInt32(row[3]),
-                row[4] == DBNull.Value ? 0 : Convert.ToInt32(row[4]),
-                Convert.ToDateTime(row[5]),
-                Convert.ToInt32(row[6]),
-                Convert.ToInt32(row[7])
-            );
-            comments.Add(comment);
+            var dataTable = _dataLink.ExecuteReader("GetReplies", parameters);
+
+            foreach (System.Data.DataRow row in dataTable.Rows)
+            {
+                Comment comment = new Comment(
+                    Convert.ToInt32(row[0]),
+                    row[1]?.ToString() ?? string.Empty,
+                    Convert.ToInt32(row[2]),
+                    Convert.ToInt32(row[3]),
+                    row[4] == DBNull.Value ? 0 : Convert.ToInt32(row[4]),
+                    Convert.ToDateTime(row[5]),
+                    Convert.ToInt32(row[6]),
+                    Convert.ToInt32(row[7])
+                );
+                comments.Add(comment);
+            }
+            return comments;
         }
-        return comments;
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public bool IncrementLikeCount(int parentCommentId)
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new SqlParameter("@CommentID", parentCommentId)
+        };
+        try
+        {
+            _dataLink.ExecuteNonQuery("IncrementLikeCount", parameters);
+            return true;
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
 }
