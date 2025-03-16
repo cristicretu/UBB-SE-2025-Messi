6bc7995c8743175843e0896a7e1468997dd401fd
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-03-16 10:17:26 +0200

    feat: likebutton heart shaped

--- Duo/Views/Components/LikeButton.xaml.cs
+++ Duo/Views/Components/LikeButton.xaml.cs
@@ -1,14 +1,28 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System.Collections.Generic;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Input;
 
 namespace Duo.Views.Components
 {
     public sealed partial class LikeButton : UserControl
     {
+        private SolidColorBrush _heartDefaultBrush;
+        private SolidColorBrush _heartHoverBrush;
+        private SolidColorBrush _heartFilledBrush;
+
         public LikeButton()
         {
             this.InitializeComponent();
+            
+            // Store brushes for easy access
+            _heartDefaultBrush = Resources["HeartDefaultBrush"] as SolidColorBrush;
+            _heartHoverBrush = Resources["HeartHoverBrush"] as SolidColorBrush;
+            _heartFilledBrush = Resources["HeartFilledBrush"] as SolidColorBrush;
+            
+            // Register event handlers for the icon's parent (button)
+            likeButton.PointerEntered += LikeButton_PointerEntered;
+            likeButton.PointerExited += LikeButton_PointerExited;
         }
 
         // Like Count Property
@@ -19,6 +33,56 @@ namespace Duo.Views.Components
         {
             get { return (int)GetValue(LikeCountProperty); }
             set { SetValue(LikeCountProperty, value); }
+        }
+
+        // Is Liked Property
+        public static readonly DependencyProperty IsLikedProperty =
+            DependencyProperty.Register(nameof(IsLiked), typeof(bool), typeof(LikeButton), new PropertyMetadata(false, OnIsLikedChanged));
+
+        public bool IsLiked
+        {
+            get { return (bool)GetValue(IsLikedProperty); }
+            set { SetValue(IsLikedProperty, value); }
+        }
+
+        private static void OnIsLikedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as LikeButton;
+            if (control != null)
+            {
+                control.UpdateHeartAppearance();
+            }
+        }
+
+        private void UpdateHeartAppearance()
+        {
+            // Only update the heart icon, not the entire button
+            if (IsLiked)
+            {
+                heartIcon.Foreground = _heartFilledBrush;
+            }
+            else
+            {
+                heartIcon.Foreground = _heartDefaultBrush;
+            }
+        }
+
+        private void LikeButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (!IsLiked)
+            {
+                // Only change the heart icon color
+                heartIcon.Foreground = _heartHoverBrush;
+            }
+        }
+
+        private void LikeButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (!IsLiked)
+            {
+                // Reset to default color when not hovering
+                heartIcon.Foreground = _heartDefaultBrush;
+            }
         }
 
         private void LikeButton_Click(object sender, RoutedEventArgs e)
