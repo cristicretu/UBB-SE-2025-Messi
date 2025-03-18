3efbb22b80facbbecdd74384fc2b198f50716dca
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-03-18 19:02:42 +0200

    dialog show

--- Duo/Views/Pages/PostDetailPage.xaml.cs
+++ Duo/Views/Pages/PostDetailPage.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Duo.Views.Pages
 {
@@ -31,16 +32,62 @@ namespace Duo.Views.Pages
             }
         }
 
-        private void MoreOptions_EditClicked(object sender, RoutedEventArgs e)
+        private async void MoreOptions_EditClicked(object sender, RoutedEventArgs e)
         {
-            // Handle edit action
-            // For now, just show a message dialog
+            // Handle the edit logic here
+
+            bool succesfullyEdited = true; // Placeholder for actual logic
+            
+            if (succesfullyEdited)
+            {
+                // Send/confirm
+                ContentDialog successDialog = new ContentDialog
+                {
+                    XamlRoot = this.XamlRoot,
+                    Title = "Edited",
+                    Content = "The item has been successfully edited.",
+                    CloseButtonText = "OK"
+                };
+                await successDialog.ShowAsync();
+            } else {
+                // Handle the error logic here
+                ContentDialog errorDialog = new ContentDialog
+                {
+                    XamlRoot = this.XamlRoot,
+                    Title = "Error",
+                    Content = "An error occurred while editing the item. Please try again.",
+                    CloseButtonText = "OK"
+                };
+                await errorDialog.ShowAsync();
+            }
         }
 
-        private void MoreOptions_DeleteClicked(object sender, RoutedEventArgs e)
+        private async void MoreOptions_DeleteClicked(object sender, RoutedEventArgs e)
         {
-            // Handle delete action
-            // For now, just show a confirmation dialog
+            // Instantiate a DeleteDialog
+            var deleteDialog = new DialogComponent();
+
+            // Create the deletion confirmation (add whatever text you wish to display)
+            bool isConfirmed = await deleteDialog.ShowConfirmationDialog(
+                "Confirm Deletion",
+                "Are you sure you want to delete this item?",
+                this.XamlRoot
+            );
+
+            // if User confirms...
+            if (isConfirmed)
+            {
+                // Handle the deletion logic here
+                // Send/confirm
+                ContentDialog successDialog = new ContentDialog
+                {
+                    XamlRoot = this.XamlRoot,
+                    Title = "Deleted",
+                    Content = "The item has been successfully deleted.",
+                    CloseButtonText = "OK"
+                };
+                await successDialog.ShowAsync();
+            }
         }
 
         private void DisplayPostDetails()
