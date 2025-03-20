ae140aa636a7524b64d1edeb3a7e75ddeb081b30
Grancea Alexandru <103886681+Nanu25@users.noreply.github.com>
2025-03-20 16:24:23 +0200

    PaginationCategories (#45)
    * d
    
    * Update Duo/Data/Queries/Post/GetPostsByCategory/query.sql
    
    ---------
    
    Co-authored-by: Cristian Crețu <crisemcr@gmail.com>

--- Duo/Data/Queries/Post/GetPostsByCategory/query.sql
+++ Duo/Data/Queries/Post/GetPostsByCategory/query.sql
@@ -1,13 +1,26 @@
-CREATE OR ALTER GetPostsByCategory (
-    @CategoryID INT,
+CREATE PROCEDURE GetPostsByCategory
+  @Offset INT,
     @PageSize INT,
-    @Offset INT
-) AS
-BEGIN 
-    SELECT Id, Title, Description, UserID, CategoryID, CreatedAt, UpdatedAt, LikeCount
-    FROM Posts
-    WHERE CategoryID = @CategoryID
-    ORDER BY CreatedAt DESC
-    OFFSET @Offset ROWS
-    FETCH NEXT @PageSize ROWS ONLY
-END
+    @CategoryID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        p.Id, 
+        p.Title, 
+        p.Description, 
+        p.UserID, 
+        p.CategoryID, 
+        p.CreatedAt, 
+        p.UpdatedAt, 
+        p.LikeCount, 
+        u.Username, 
+        c.CategoryName
+    FROM Posts p
+    JOIN Users u ON p.UserID = u.Id  
+    JOIN Categories c ON p.CategoryID = c.Id 
+    WHERE p.CategoryID = @CategoryID
+    ORDER BY p.CreatedAt DESC  
+    OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;
+END;
