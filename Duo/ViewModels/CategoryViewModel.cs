4a5ed5f1253c69316f1c2b57ba5337fa0db6a603
Cristian Cretu <crisemcr@gmail.com>
2025-03-21 08:48:46 +0200

    feat: get categories from the db

---
+++ Duo/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Windows.Input;
+using Duo.Models;
+using Duo.Services;
+using Microsoft.UI.Xaml;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Duo.Commands;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Duo.ViewModels
+{
+    public class CategoryViewModel : INotifyPropertyChanged
+    {
+        private readonly CategoryService _categoryService;
+        private string _categoryName = string.Empty;
+        private List<Category> _categories = new List<Category>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler LoginSuccessful;
+
+        public string CategoryName
+        {
+            get => _categoryName;
+            set
+            {
+                if (_categoryName != value)
+                {
+                    _categoryName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<Category> Categories
+        {
+            get => _categories;
+            set
+            {
+                if (_categories != value)
+                {
+                    _categories = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public CategoryViewModel(CategoryService categoryService)
+        {
+            _categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
+            LoadCategories();
+        }
+
+        public void LoadCategories()
+        {
+            try
+            {
+                Categories = _categoryService.GetAllCategories();
+                Debug.WriteLine($"Successfully loaded {Categories.Count} categories from service");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to load categories: {ex.Message}");
+                Categories = new List<Category>();
+            }
+        }
+
+        public List<string> GetCategoryNames()
+        {
+            List<string> categoryNames = new List<string>();
+            foreach (var category in Categories)
+            {
+                categoryNames.Add(category.Name);
+            }
+            return categoryNames;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
